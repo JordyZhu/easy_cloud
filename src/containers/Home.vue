628721a707c1ad3b42b2256ba24936469cb4e4d6
zhujie <zhujie@zhihu.com>
2017-06-12 13:41:53 +0800

    some fix

--- src/containers/Home.vue
+++ src/containers/Home.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="Home">
+    {{ content }}
   </div>
 </template>
 
 <script>
+  import {Http} from 'vue-resource'
+
   export default {
     name: 'home',
     data () {
       return {}
+    },
+    computed: {
+      content () {
+        Http.get('http://www.baidu.com').then(response => {
+          return response.body
+        })
+      }
     }
   }
 </script>
