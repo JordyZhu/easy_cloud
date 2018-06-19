f03e9ec15904dab8c8a9e23583364b15e260a352
zhujie <zhujie@zhihu.com>
2018-06-19 19:08:01 +0800

    nothing

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,6 @@ export const MY_NAME = 'Jie Zhu'
 
 export const MY_DESC = 'Front-End Software Engineer / Designer'
 
-export const MY_PHONE = '17710146808'
+export const MY_PHONE = '17610188822'
 
 export const MY_EMAIL = 'zj930210@outlook.com'
