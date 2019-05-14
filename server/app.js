6dea6cc9e9f8779905d3e52a0bf13c3d3fd8504d
gabriel.zhu <gabriel.zhu@nio.com>
2019-05-15 00:00:51 +0800

    fix: router bug

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(router)
 app.use('/', express.static(path.join(__dirname, '../dist')))
+app.use(/^(?!\/(api).*$).*/, express.static(path.join(__dirname, '../dist/index.html')))
 
 process.on('uncaughtException', err => {
   console.error('An uncaught error occurred!')
